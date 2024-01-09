4a339ad457f288a2121542fd18d51ec3c0a37b1b
Jarpekulo <112931370+Jarpekulo@users.noreply.github.com>
2024-01-09 14:03:12 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+function WinOpen_z2()
+{
+window.open("zadanie2_syk.html","okienko_z2","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z3()
+{
+window.open("zadanie3_syk.html","okienko_z3","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z4()
+{
+window.open("zadanie4_syk.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+
+
+
+
+function WinOpen_z2_kod()
+{
+window.open("zadanie2_syk-KOD.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z3_kod()
+{
+window.open("zadanie3_syk-KOD.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+function WinOpen_z4_kod()
+{
+window.open("zadanie4_syk-KOD.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=600,width=1200,top=200,left=200");
+}
+
